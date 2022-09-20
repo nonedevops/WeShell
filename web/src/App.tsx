2506a99c7bfcfd6396eae880704e4b65ad869af0
yishuiwang <yishuiwang163@gmail.com>
2022-09-20 15:01:10 +0800

    update icon

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Button from '@mui/material/Button';
 import Dashboard from  "../src/components/DashBoard/Dashboard";
 function App() {
   return (
     <div className="App">
         <div>
         <Dashboard></Dashboard>
-
+        
         </div>
     </div>
   );
--- web/src/components/DashBoard/listItems.tsx
+++ web/src/components/DashBoard/listItems.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
+import SettingsIcon from '@mui/icons-material/Settings';
+import DriveFileMoveIcon from '@mui/icons-material/DriveFileMove';
+import TerminalIcon from '@mui/icons-material/Terminal';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
+import LogoutIcon from '@mui/icons-material/Logout';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PeopleIcon from '@mui/icons-material/People';
@@ -20,53 +24,39 @@ export const mainListItems = (
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
-        <ShoppingCartIcon />
+        <TerminalIcon />
       </ListItemIcon>
-      <ListItemText primary="Orders" />
+      <ListItemText primary="SSH" />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
-        <PeopleIcon />
+        <SettingsIcon />
       </ListItemIcon>
-      <ListItemText primary="Customers" />
+      <ListItemText primary="Config" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <DriveFileMoveIcon />
+      </ListItemIcon>
+      <ListItemText primary="Sftp" />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
         <BarChartIcon />
       </ListItemIcon>
-      <ListItemText primary="Reports" />
+      <ListItemText primary="Extend" />
     </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItemButton>
+    
   </React.Fragment>
 );
 
 export const secondaryListItems = (
   <React.Fragment>
-    <ListSubheader component="div" inset>
-      Saved reports
-    </ListSubheader>
-    <ListItemButton>
+    <ListItemButton style={{marginBottom:'0%'}}>
       <ListItemIcon>
-        <AssignmentIcon />
+        <LogoutIcon />
       </ListItemIcon>
-      <ListItemText primary="Current month" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Year-end sale" />
+      <ListItemText primary="Logout Out" />
     </ListItemButton>
   </React.Fragment>
 );
